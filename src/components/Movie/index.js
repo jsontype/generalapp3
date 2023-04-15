7c83959d5fad1c37c040d10e5a5409f8c43bbad7
jsontype <jsontyper@gmail.com>
2023-04-15 16:44:29 +0900

    feat: 색깔 평점 적용

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react"
+import "./style.css"
 
 export default function Movie() {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
+    // fetch("https://yts.mx/api/v2/list_movies.json") // Ref: 평점 색깔 테스트 용
     fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
       .then((res) => res.json())
       .then((json) => setMovies(json.data.movies))
@@ -12,12 +14,17 @@ export default function Movie() {
   console.log(movies)
 
   const render = movies.map((item) => {
+    const ratingStatus =
+      item.rating >= 9 ? "good" : item.rating >= 7 ? "normal" : "bad"
+
     return (
       <div key={item.id}>
         <div>
           {item.title} [{item.year}]
         </div>
-        <div>평점: {item.rating} / 10점</div>
+        <div>
+          평점: <span className={ratingStatus}>{item.rating}</span> / 10점
+        </div>
         <img src={item.medium_cover_image} alt={item.title}></img>
       </div>
     )
